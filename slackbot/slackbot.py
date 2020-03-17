fc8c222e041dd7e4633e0eaa7d2820c44b2a9497
Julius Schulz <58485703+Julius1812@users.noreply.github.com>
2020-03-17 17:25:00 +0100

    add slackbot.py

---
+++ slackbot/slackbot.py
@@ -0,0 +1,35 @@
+import slack
+from sqlalchemy import create_engine
+import time
+import pandas as pd
+
+oauth_token ="xoxb-892207304208-960672730260-RTLKi8D89ibOsRIXDaHoLom9"
+
+def slack_text(slack_message):
+    client = slack.WebClient(token=oauth_token)
+    #def slack_text(slack_message):
+    response = client.chat_postMessage(channel='#random', text=slack_message)
+
+
+def get_text():
+    HOST= 'postgresdb'
+    PORT= '5432'
+    USERNAME= 'postgres'
+    PASSWORD= 'postgres'
+    DB = 'tweetdb'
+    conn_string = f'postgres://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB}'
+
+    engine = create_engine(conn_string)
+    query = "SELECT text FROM tweets LIMIT 1;"
+    result = engine.execute(query)
+    for text in result:
+        print(text)
+    return text[0]
+
+
+while True:
+    time.sleep(40)
+    slack_message = get_text()
+    print('slack_message')
+    slack_text(slack_message)
+    time.sleep(30)
