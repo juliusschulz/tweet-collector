2c3bbbcad0156a7ca19401d861dfc3dcc88a28b4
Julius Schulz <58485703+Julius1812@users.noreply.github.com>
2020-03-18 19:23:35 +0100

    add test_extract_new_tweets + test_transform_date

---
+++ tests/test_etl_job.py
@@ -0,0 +1,18 @@
+import time
+import pymongo
+from sqlalchemy import create_engine
+from pymongo import MongoClient
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyze
+import etl_job
+import pandas as pd
+
+
+def test_extract_new_tweets():
+    """ Test the connection to MongoDB by checking
+    whether list of extracted tweets is >= 0 """
+    assert len(extract_new_tweets()) >= 0
+
+def test_transform_data():
+    df = transform_data()
+    count = df.isnull().count()
+    assert count == 0
